317d1e74dd418d161eef9d28567c9d2224ae9852
Fabio Rodrigues <fabio.rodrigues018@gmail.com>
2019-06-21 23:51:32 -0300

    adding tests to reservation

--- ddd/domain/reservation/reservation_test.go
+++ ddd/domain/reservation/reservation_test.go
@@ -5,11 +5,25 @@ import (
 	"testing"
 
 	"github.com/fabiorodrigues/gostructure/ddd/domain"
+	"github.com/fabiorodrigues/gostructure/ddd/domain/entities"
 	"github.com/fabiorodrigues/gostructure/ddd/infra"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestReserve(t *testing.T) {
-	t.Run("should return error when not", func(t *testing.T) {
+	t.Run("should return error when got an error when getting book", func(t *testing.T) {
+		mockBookRepo := domain.MockBookRepository{
+			GetByIDfn: func(ID string) (entities.Book, error) {
+				return entities.Book{}, errors.New("error getting book")
+			},
+		}
+
+		service := NewService(mockBookRepo, infra.EmailSenderMock{})
+
+		err := service.Reserve("123")
+		assert.Equal(t, "error getting book", err.Error())
+	})
+	t.Run("should return error when not sending email", func(t *testing.T) {
 		mockEmailSender := infra.EmailSenderMock{
 			Sendfn: func(opts infra.EmailOptions) error {
 				return errors.New("error when sending email")
@@ -17,5 +31,7 @@ func TestReserve(t *testing.T) {
 		}
 
 		service := NewService(domain.MockBookRepository{}, mockEmailSender)
+		err := service.Reserve("123")
+		assert.Equal(t, "error when sending email", err.Error())
 	})
 }
--- ddd/go.mod
+++ ddd/go.mod
@@ -2,4 +2,7 @@ module github.com/fabiorodrigues/gostructure/ddd
 
 go 1.12
 
-require github.com/ditointernet/go-assert v0.0.0-20190308171610-c7953af9e469
+require (
+	github.com/ditointernet/go-assert v0.0.0-20190308171610-c7953af9e469
+	github.com/stretchr/testify v1.3.0
+)

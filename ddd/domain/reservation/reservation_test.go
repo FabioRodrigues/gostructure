07622175f19d8cac3346ae3a4801de1574674265
Fabio Rodrigues <fabio.rodrigues018@gmail.com>
2019-06-21 23:41:51 -0300

    adding tests to reservation

--- ddd/domain/reservation/reservation_test.go
+++ ddd/domain/reservation/reservation_test.go
@@ -1,13 +1,21 @@
 package reservation
 
 import (
+	"errors"
 	"testing"
+
+	"github.com/fabiorodrigues/gostructure/ddd/domain"
+	"github.com/fabiorodrigues/gostructure/ddd/infra"
 )
 
 func TestReserve(t *testing.T) {
-	// t.Run("should return error when not", func(t *testing.T) {
-	// 	mock := infra.EmailSenderMock{
-	// 		Sendfn: func,
-	// 	}
-	// })
+	t.Run("should return error when not", func(t *testing.T) {
+		mockEmailSender := infra.EmailSenderMock{
+			Sendfn: func(opts infra.EmailOptions) error {
+				return errors.New("error when sending email")
+			},
+		}
+
+		service := NewService(domain.MockBookRepository{}, mockEmailSender)
+	})
 }

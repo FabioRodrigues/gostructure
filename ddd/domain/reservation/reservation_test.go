2c6568cfffb8a56f1f5e4c31ee61c26f3bd4fc89
Fabio Rodrigues <fabio.rodrigues018@gmail.com>
2019-06-21 23:30:28 -0300

    adding tests to reservation

--- ddd/domain/reservation/reservation_test.go
+++ ddd/domain/reservation/reservation_test.go
@@ -1,1 +1,13 @@
 package reservation
+
+import (
+	"testing"
+)
+
+func TestReserve(t *testing.T) {
+	// t.Run("should return error when not", func(t *testing.T) {
+	// 	mock := infra.EmailSenderMock{
+	// 		Sendfn: func,
+	// 	}
+	// })
+}
